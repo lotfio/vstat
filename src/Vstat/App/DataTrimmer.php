81f876a54e45300850e6b3899eab8cd2af6d635a
ABD ELLATIF LAKEHAL <lotfiolakehal@gmail.com>
2019-01-04 15:55:16 +0000

    Apply fixes from StyleCI

--- src/Vstat/App/DataTrimmer.php
+++ src/Vstat/App/DataTrimmer.php
@@ -1,7 +1,9 @@
-<?php namespace Vstat\App;
+<?php
 
-use Vstat\Contracts\DataTrimmerInterface;
+namespace Vstat\App;
+
 use Vstat\Contracts\DataParserInterface;
+use Vstat\Contracts\DataTrimmerInterface;
 
 /**
  * Vstat is an open source PHP API That
@@ -9,50 +11,42 @@ use Vstat\Contracts\DataParserInterface;
  * (Virtual Air Traffic Simulation Network)
  * This package is developed and maintained
  * by lotfio lakehal.
- * 
- * @package     Vstat
+ *
  * @version     0.1.0
+ *
  * @author      Lotfio Lakehal <contact@lotfio.net>
  * @copyright   Lotfio Lakehal 2018
  * @license     MIT
+ *
  * @link        https://github.com/lotfio/vstat
- * 
  */
-
 class DataTrimmer implements DataTrimmerInterface
 {
-	
     /**
-     * 
      * @method  trim method
-     * 
-     * @param   $data vatsim data array
-     * @param   $startLine start trimming line
+     *
+     * @param   $data       vatsim data array
+     * @param   $startLine  start trimming line
      * @param   $dataParser data parser object
-     * @param   $method data parser method
-     * 
+     * @param   $method     data parser method
      */
-	public function trim(array $data,string $startLine,DataParserInterface $dataParser,string $method) : array
+    public function trim(array $data, string $startLine, DataParserInterface $dataParser, string $method) : array
     {
         $allowed = false;
         $records = [];
 
         foreach ($data as $line) {
+            if (substr($line, 0, 1) != ';') { // not a comment
 
-            if (substr($line, 0, 1) != ";") { // not a comment
+                $line = iconv('ISO-8859-1', 'UTF-8', trim($line));
 
-                $line = iconv("ISO-8859-1", "UTF-8", trim($line));
-
-
-                if ($allowed == true && substr($line, 0, 1) != "!") {
-
-                    $records[] = (object) call_user_func_array([$dataParser,$method], array(explode(":", $line)));
-
+                if ($allowed == true && substr($line, 0, 1) != '!') {
+                    $records[] = (object) call_user_func_array([$dataParser, $method], [explode(':', $line)]);
                 } else {
-
                     $allowed = false;
-                    if ($line == $startLine) $allowed = true; // $startLine = "!SERVER:"
-
+                    if ($line == $startLine) {
+                        $allowed = true;
+                    } // $startLine = "!SERVER:"
                 }
             }
         }
--- tests/Unit/DataTrimmerTest.php
+++ tests/Unit/DataTrimmerTest.php
@@ -1,7 +1,10 @@
-<?php namespace Tests\Unit;
+<?php
+
+namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use Vstat\App\{DataTrimmer, DataParser};
+use Vstat\App\DataParser;
+use Vstat\App\DataTrimmer;
 
 /**
  * Vstat is an open source PHP API That
@@ -9,31 +12,29 @@ use Vstat\App\{DataTrimmer, DataParser};
  * (Virtual Air Traffic Simulation Network)
  * This package is developed and maintained
  * by lotfio lakehal.
- * 
- * @package     Vstat
+ *
  * @version     0.1.0
+ *
  * @author      Lotfio Lakehal <contact@lotfio.net>
  * @copyright   Lotfio Lakehal 2018
  * @license     MIT
+ *
  * @link        https://github.com/lotfio/vstat
- * 
  */
-
 class DataTrimmerTest extends TestCase
 {
-	
-	public function setUp()
-	{
-		$this->dataTrimmer = new DataTrimmer;
-		$this->parser = new DataParser;
-	}
+    public function setUp()
+    {
+        $this->dataTrimmer = new DataTrimmer();
+        $this->parser = new DataParser();
+    }
 
-	public function testTrimMethod()
-	{
-		$data = file(__DIR__ . '/Stabs/vatsim-data.txt');
-		$from = "!CLIENTS:";
-		$res  = $this->dataTrimmer->trim($data,$from,$this->parser, "clientsParser");
-		$this->assertInternalType('array',$res);
-		$this->assertInternalType('object',$res[0]);
-	} 
+    public function testTrimMethod()
+    {
+        $data = file(__DIR__.'/Stabs/vatsim-data.txt');
+        $from = '!CLIENTS:';
+        $res = $this->dataTrimmer->trim($data, $from, $this->parser, 'clientsParser');
+        $this->assertInternalType('array', $res);
+        $this->assertInternalType('object', $res[0]);
+    }
 }
