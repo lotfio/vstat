3165e2ed26357519c8c1dc668cc3d42679046b3c
ABD ELLATIF LAKEHAL <lotfiolakehal@gmail.com>
2019-01-04 14:46:08 +0000

    Apply fixes from StyleCI

--- tests/Unit/DataParserTest.php
+++ tests/Unit/DataParserTest.php
@@ -1,4 +1,6 @@
-<?php namespace Tests\Unit;
+<?php
+
+namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Vstat\App\DataParser;
@@ -9,66 +11,62 @@ use Vstat\App\DataParser;
  * (Virtual Air Traffic Simulation Network)
  * This package is developed and maintained
  * by lotfio lakehal.
- * 
- * @package     Vstat
+ *
  * @version     0.1.0
+ *
  * @author      Lotfio Lakehal <contact@lotfio.net>
  * @copyright   Lotfio Lakehal 2018
  * @license     MIT
+ *
  * @link        https://github.com/lotfio/vstat
- * 
  */
-
 class DataParserTest extends TestCase
 {
-	public $clientsData = [ 
-		'AAL2135','1315456','Neal Shamsudeen YMML', 'PILOT',
-		'00','42.36083','-71.01946','34','0','A320','410','KBOS','22000',
-		'KLGA','AUSTRALIA','100','1','2000','7','7','2','I','1300','1300',
-		'0','45','2','4','KBOS','+VFPS+/V/PLANNED OPTIMUM FLIGHT LEVEL',
-		'PATSS DCT NELIE DCT VALRE','0','0','0','0','0','0', '20181211103150',
-		'92','30.189','221'];
+    public $clientsData = [
+        'AAL2135', '1315456', 'Neal Shamsudeen YMML', 'PILOT',
+        '00', '42.36083', '-71.01946', '34', '0', 'A320', '410', 'KBOS', '22000',
+        'KLGA', 'AUSTRALIA', '100', '1', '2000', '7', '7', '2', 'I', '1300', '1300',
+        '0', '45', '2', '4', 'KBOS', '+VFPS+/V/PLANNED OPTIMUM FLIGHT LEVEL',
+        'PATSS DCT NELIE DCT VALRE', '0', '0', '0', '0', '0', '0', '20181211103150',
+        '92', '30.189', '221', ];
 
-	public $serversData  = ['123.55.1.3','T','4','A8',''];
+    public $serversData = ['123.55.1.3', 'T', '4', 'A8', ''];
 
-	/**
-	 * setting up our test object
-	 */
-	public function setUp()
-	{
-		$this->parser = new DataParser;
-	}
+    /**
+     * setting up our test object.
+     */
+    public function setUp()
+    {
+        $this->parser = new DataParser();
+    }
 
+    /**
+     * test clients parser method is returning
+     * a valid array of data with 41 element.
+     */
+    public function testClientsParserIsReturningAvalidArray()
+    {
+        $this->assertInternalType('array', $this->parser->clientsParser($this->clientsData));
+        $this->assertCount(41, $this->parser->clientsParser($this->clientsData));
+    }
 
-	/**
-	 * test clients parser method is returning 
-	 * a valid array of data with 41 element
-	 */
-	public function testClientsParserIsReturningAvalidArray()
-	{
-		$this->assertInternalType('array', $this->parser->clientsParser($this->clientsData));
-		$this->assertCount(41, $this->parser->clientsParser($this->clientsData));
-	}	
+    /**
+     * test Servers parser method is returning
+     * a valid array of data with 5 element.
+     */
+    public function testServersParserIsReturningAvalidArray()
+    {
+        $this->assertInternalType('array', $this->parser->serversParser($this->serversData));
+        $this->assertCount(5, $this->parser->serversParser($this->serversData));
+    }
 
-	/**
-	 * test Servers parser method is returning 
-	 * a valid array of data with 5 element
-	 */
-	public function testServersParserIsReturningAvalidArray()
-	{
-		$this->assertInternalType('array', $this->parser->serversParser($this->serversData));
-		$this->assertCount(5, $this->parser->serversParser($this->serversData));
-	}
-
-	/**
-	 * test voiceServers parser method is returning 
-	 * a valid array of data with 5 element
-	 */
-	public function testVoiceServersParserIsReturningAvalidArray()
-	{
-		$this->assertInternalType('array', $this->parser->voiceServersParser($this->serversData));
-		$this->assertCount(5, $this->parser->voiceServersParser($this->serversData));
-	}
-
-
+    /**
+     * test voiceServers parser method is returning
+     * a valid array of data with 5 element.
+     */
+    public function testVoiceServersParserIsReturningAvalidArray()
+    {
+        $this->assertInternalType('array', $this->parser->voiceServersParser($this->serversData));
+        $this->assertCount(5, $this->parser->voiceServersParser($this->serversData));
+    }
 }
